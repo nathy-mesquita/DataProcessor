8648286b92bd6ffa4810cc456ed3925c2b47adca
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2022-11-30 04:19:56 -0300

    feat: modulo 2

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataProcessor;
+using static System.Console;
+
+//Command line validation ommited for brevity
+var command = args[0];
+
+
+/* command --file in Debug properties
+ *  --file c:\psdata\in\data1.txt or "commandLineArgs": "--file c:\\psdata\\in\\data1.txt"
+ * 
+ * commad --dir in Debug properties
+ * --dir c:\psdata\in TEXT or "commandLineArgs": "--dir c:\\psdata\\in TEXT"
+ */
+if (command == "--file")
+{
+    var filePath = args[1];
+
+    //Check if path is absolute
+    if (!Path.IsPathFullyQualified(filePath))
+    {
+        WriteLine($"ERROR: path '{filePath}' must be fully qualified.");
+        ReadLine();
+        return;
+    }
+    WriteLine($"Single file {filePath} selected");
+    ProcessSigleFile(filePath);
+
+}
+else if (command == "--dir")
+{
+    var directoryPath = args[1];
+    var fileType = args[2];
+    WriteLine($"Directory {directoryPath} selected for {fileType} files");
+    ProcessDirectory(directoryPath, fileType);
+}
+else
+{
+    Write("Invalid command line options");
+}
+WriteLine("Press enter to quit.");
+ReadLine();
+
+static void ProcessSigleFile(string filePath)
+{
+    var fileProcessor = new FileProcessor(filePath);
+    fileProcessor.Process();
+}
+static void ProcessDirectory(string directoryPath, string fileType)
+{
+    switch (fileType)
+    {
+        case "TEXT":
+            string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+            foreach (var textFilePath in textFiles)
+            {
+                var fileProcessor = new FileProcessor(textFilePath);
+                fileProcessor.Process();
+            }
+            break;
+        default:
+            WriteLine($"ERROR: {fileType} is not supported");
+            return;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # DataProcessor
+## Working with Files in C# 10 
+
+### 1 - Managing Files and Directories
+Verificar se um arquivo existe ou não, utilizando o método estático da classe File:
+
+```csharp
+File.Exists()
+```
+
+
+
+Verificar a existência de um diretório utilizando o método estático da classe Directory:
+
+```csharp
+Directory.Exists()
+```
+
+> PS: A classe File e Directory existe no namespace system.io. 
+
+Criar um novo diretório:
+
+```csharp
+Directory.CreateDirectory()
+```
+
+Previnir lançamentos de  exceção se o diretório não existir.
+
+```csharp
+if (!Directory.Exists(backupDirectoryPath))
+{
+    WriteLine($"Creating {backupDirectoryPath}");
+    Directory.CreateDirectory(backupDirectoryPath);
+}
+```
+
+Copiar arquivos:
+
+```csharp
+File.Copy()
+```
+
+Mover arquivos:
+
+```csharp
+File.Move()
+```
+
+Deletar o diretório 
+
+```csharp
+Directory.Delete()
+```
+
+Obter uma lista de arquivos em um diretório:
+
+```csharp
+Directory.GetFiles()
+```
+
+Utilidade do `Path.Combine()` da classe `Path` que também existe no namespace system.io.
+
+que nos permite construir um caminho inteiro com base em fragmentos de caminhos.
+
+Obter apenas a extensão do arquivo:
+
+```csharp
+Path.GetExtension()
+```
+
+Alterar a extensão do arquivo:
+
+```csharp
+Path.ChangeExtension()
+```
